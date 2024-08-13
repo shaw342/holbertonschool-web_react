f6b6f9c0aa017bbe9b6e1b634a164f48a2435227
shaw342 <shawanbarua70@gmail.com>
2024-08-13 16:10:14 +0200

    task 3 implement printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,11 +11,13 @@ interface Directors extends Teacher{
     numberOfReports: Number
 }
 
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
-  console.log(director1);
+interface printTeacherFunction{
+    readonly firstName:string,
+    readonly lastName: string,
+}
+
+function printTeacher(firstName: string, lastName: string,): string {
+    return `${firstName[0]}.${lastName}`
+}
+
+console.log(printTeacher("John", "Doe"));
