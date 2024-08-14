a25d26286db27d4e5c623a78b44d8e0ece6d1f0d
shaw342 <shawanbarua70@gmail.com>
2024-08-14 20:34:08 +0200

    task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,4 +1,4 @@
-interface DirectorsInteface{
+interface DirectorInterface {
     workFromHome():string,
     getCoffeeBreak():string,
     workDirectorTasks():string
@@ -21,14 +21,40 @@ class Teacher implements TeacherInterface{
         return "Getting to work"
     }
 }
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to director tasks"
+    }
+}
 
-function createEmployee(salary: string | number): string {
+function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+    // Extraire et convertir la valeur numérique de la chaîne
     const numericSalary = typeof salary === 'number' ? salary : parseFloat(salary.toString().replace(/[^0-9.-]+/g, ''));
 
     if (typeof numericSalary === 'number' && numericSalary < 500) {
-        return "Teacher"
+        return new Teacher();
     } else {
-        return "Director"
+        return new Director();
     }
 }
-console.log(createEmployee('$500'));
+
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+    return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
