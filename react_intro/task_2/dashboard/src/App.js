1d64269021c8852112f76248c85c4acdb4b994a9
shaw342 <shawanbarua70@gmail.com>
2024-08-26 15:57:52 +0200

    handle css for form

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -7,20 +7,21 @@ function App() {
     
      <>
      <div className="App-header">
-      <img src={logo}/>
+      <img src={logo} alt="logo"/>
       <h1>
       School dashboard
       </h1>
     </div>
     <div className="App-body">
       <p>Login to access the full dashboard</p>
-      <label for="email">
-          <input type="email" id="email" name="email"/>
-        </label>
-        <label for="password">your password ?</label>
-        <input type="password" id="password" name="password"/>
-        
+
+      <div className="App-body_container">
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
         <button type="submit">OK</button>
+      </div>
       </div>
       <div className="App-footer" >
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {getLatestNotification} from "./utils";
+import './Notifications.css';
 
 export default function Notification(){
+
+    function HandleClick(e){
+        e.preventDefault();
+        console.log("Close button has been clicked");
+        
+    }
     return (
         <div className="Notifications">
         <p>Here is the list of notifications</p>
+        <button aria-label="Close" onClick={HandleClick}>x
+        </button>
+        <ul>
+            <li data-priority="default">New course available</li>
+            <li data-priority="urgent">New resume available</li>
+            <li dangerouslySetInnerHTML={{__html: getLatestNotification()}} data-priority="urgent"></li>
+        </ul>
         </div>
     )
 }
