152555b0b1d7a78b68899599f6d54ca5d73b17e1
shaw342 <shawanbarua70@gmail.com>
2024-09-08 20:37:43 +0200

    task 0

---
+++ React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Notifications.css'
+import { getLatestNotification } from '../utils/utils';
+import closeIcon from '../assets/close-icon.jpg'
+import NotificationItem from './NotifiactionItem';
+
+function Notifications() {
+    return (
+    <div className="Notifications" style={{ position: 'relative' }}>
+      <button
+        style={{ position: 'absolute', right: '10px', top: '10px' }}
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img src={closeIcon} alt="Close" />
+      </button>
+        <p>
+            Here is the list of notifications
+        </p>
+        <ul>
+          <NotificationItem value="New course available" type='urgent'/>
+          <NotificationItem value="New course available" type='urgent'/>
+          <NotificationItem value="New course available" type='urgent' html={{__html:getLatestNotification()}}/>
+      </ul>
+    </div>
+    )
+}
+
+export default Notifications;
