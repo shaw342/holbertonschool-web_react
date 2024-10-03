7bf3a020b8f556b10bc515597b6a9eecc52ab497
shaw342 <shawanbarua70@gmail.com>
2024-10-03 13:02:55 +0200

    create store

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications/Notifications';
+import App from '../../../task_1/dashboard/src/App/App';
+import reportWebVitals from '../../../task_1/dashboard/src/reportWebVitals';
+import Notifications from '../../../task_1/dashboard/src/Notifications/Notifications';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import uiReducer from '../../../task_1/dashboard/src/reducers/uiReducer';
 
+const store = createStore(uiReducer)
 ReactDOM.render(
+
+  <Provider store={store}>
   <div>
 	<div className="root-notifications">
       <Notifications />
 	</div>
     <App />
   </div>,
+  </Provider>,
   document.getElementById('root')
 );
 
