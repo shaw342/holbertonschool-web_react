0ea7b7fc90a59b08adf1c6e6724ba44288344e5a
shaw342 <shawanbarua70@gmail.com>
2024-10-08 19:01:14 +0200

    reducers test

---
+++ react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.test.js
@@ -0,0 +1,7 @@
+import React from "react"
+import {shallow} from "enzyme"
+import { isThenable } from "next/dist/client/components/router-reducer/router-reducer-types"
+
+describe("root-reducer",()=>{
+    
+})
